85b79f4c3bd72e6be15c8f4e3b1cab91a84ff0ae
Patrick van Zadel <patrick.vanzadel@luminis.eu>
2024-11-08 19:22:22 +0100

    Added Nuxt frontend setup including Dockerfiles and docker compose

---
+++ frontend/README.md
@@ -0,0 +1,13 @@
+# Mijnclub.nu Frontend
+
+Run development through docker
+
+```bash
+docker compose -f docker-compose.dev.yml up --build
+```
+
+Run production through docker
+
+```bash
+docker compose -f docker-compose.yml up --build
+```
---
+++ frontend/components/GoogleMapsComponent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+	const isLoaded = ref(false)
+	const center = ref()
+	const maps = ref()
+
+	const query = ref({
+		lat: -37.7995487,
+		lng: 144.9867841,
+	})
+
+	const markers = ref([])
+
+	function handleReady({map}: any) {
+		center.value = map.value.getCenter()
+		map.value.addListener('center_changed', () => {
+			center.value = map.value.getCenter()
+		})
+		isLoaded.value = true
+	}
+</script>
+
+<template>
+	<div class="not-prose h-1/3">
+		<div class="w-full h-1/3">
+			<ScriptGoogleMaps
+					ref="maps"
+					:center="query"
+					:markers="markers"
+					api-key="AIzaSyDSXSomToixQF1H2aYa0JE8Kg_8eKXd7y8"
+					class="group !w-full !h-1/3"
+					above-the-fold
+					@ready="handleReady"
+			/>
+		</div>
+	</div>
+</template>
